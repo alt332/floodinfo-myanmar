ae33663207d415bd160f8d884fcabf6b31b38dae
Thiha <alt.332@gmail.com>
2015-08-04 23:38:41 +0630

    modify location admin index view and rename fetch location command

--- location/admin.py
+++ location/admin.py
@@ -3,13 +3,21 @@ from .models import Location
 
 
 class LocationAdmin(admin.ModelAdmin):
+    list_display = ('state', 'township', 'district')
     fieldsets = [
         (None, {'fields': [
             'district',
             'township',
             'latitude',
             'longitude',
-            'type',
+            'state',
+            'description',
+            'total_male',
+            'total_female',
+            'urban_total_male',
+            'urban_total_female',
+            'rural_total_male',
+            'rural_total_female',
         ]}),
     ]
 
