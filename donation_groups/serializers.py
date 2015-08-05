58560ba5e372b53b44c97405bc65a5a17ed4d470
Thiha <alt.332@gmail.com>
2015-08-05 16:03:03 +0630

    remove all html tags for titles and descriptions

--- donation_groups/serializers.py
+++ donation_groups/serializers.py
@@ -5,6 +5,8 @@ from .models import DonationGroup
 
 class DonationGroupSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
+        validated_data['title'] = strip_tags(validated_data['title'])
+        validated_data['description'] = strip_tags(validated_data['description'])
         return DonationGroup.objects.create(**validated_data)
 
 
--- newsfeed/serializers.py
+++ newsfeed/serializers.py
@@ -1,10 +1,13 @@
 from django.forms import widgets
+from django.utils.html import strip_tags
 from rest_framework import serializers
 from .models import Newsfeed
 
 
 class NewsfeedSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
+        validated_data['title'] = strip_tags(validated_data['title'])
+        validated_data['description'] = strip_tags(validated_data['description'])
         return Newsfeed.objects.create(**validated_data)
 
 
